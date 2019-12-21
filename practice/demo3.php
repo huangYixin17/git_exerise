45dfd5db364ac8d84f2389f7ceaba69db41fed85
xiz860817 <s0984008299@gmail.com>
2019-12-21 23:49:30 +0800

    繼承的執行程式

---
+++ practice/demo3.php
@@ -0,0 +1,10 @@
+<?php
+    include "Shepherd";
+
+    $mydog = new Shepherd("peter","brown","牧羊犬","10");
+    echo $mydog->name."<br/>";
+    echo $mydog->color."<br/>";
+    echo $mydog->style."<br/>";
+    echo $mydog->dowork()."<br/>";
+    echo $mydog->dogRun()."<br/>"
+?>
