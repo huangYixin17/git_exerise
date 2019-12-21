bc9156b4a3c5ddb9d824968efe56c5721a32d485
xiz860817 <s0984008299@gmail.com>
2019-12-21 20:22:21 +0800

    test

---
+++ practice/MyClass.php
@@ -0,0 +1,21 @@
+<?php
+    class MyClass{
+        public $x = 123;
+        protected $y = "spring";
+        private $z = "hello";
+
+        public function getPara(){
+            echo "inclass->x : ".$this->x."<br />";
+            echo "inclass->y : ".$this->y."<br />";
+            echo "inclass->x : ".$this->z."<br />";
+        }
+    }
+
+    $test = new MyClass();
+    $test->getPara();
+    echo "test->x : ".$test->x."<br/>";
+    echo "test->y : ".$test->y."<br/>";
+    echo "test->z : ".$test->z."<br/>";
+
+?>
+
