e15d05e0946cdd3fd84cfc0abe17d0a634c749bb
xiz860817 <s0984008299@gmail.com>
2019-12-28 12:05:58 +0800

    抽象類別使用方式

--- practice/Poodle_abst.php
+++ practice/Poodle_abst.php
@@ -4,7 +4,7 @@
     class Poodle_abst extends dog{
         protected $size;
 
-        public function __construct($name,$color,$style)
+        public function __construct($name,$color,$style,$size)
         {
             parent::__construct($name,$color,$style);
             $this->size = $size;
