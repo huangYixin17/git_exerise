1e586178cf8e0533cfdc551b51781a923c06980b
xiz860817 <s0984008299@gmail.com>
2019-12-28 12:49:48 +0800

    利用相同父類別的關西,進行不同子類別的方法實作,產生不同的解果

--- practice/Action_class.php
+++ practice/Action_class.php
@@ -2,7 +2,7 @@
     include "dog_class.php";
     include "Bird_class.php";
     include "Animal_abst.php";
-    class Action extends Dog{
+    class Action extends Animal{
         public function actionMove(Animal $obj){
             $obj->move();
         }
