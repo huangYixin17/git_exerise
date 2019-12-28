f0a2d723bdc9b2a6a9eeb21f11cb774ca8a7432c
xiz860817 <s0984008299@gmail.com>
2019-12-28 12:55:44 +0800

    利用相同父類別的關西,進行不同子類別的方法實作,產生不同的解果

--- practice/demo5.php
+++ practice/demo5.php
@@ -1,10 +1,10 @@
 <?php
-    include "Action_class.php";
-    include "dog_class.php";
+    //include "Action_class.php";
+    //include "dog_class.php";
     include "Bird_class.php";
-    $dog = new Dog();
+    //$dog = new Dog();
     $bird = new Bird();
-    $action = new Action();
+    //$action = new Action();
     $bird->move();
     //$action->actionMove($dog);
     //$action->actionMove($bird);
