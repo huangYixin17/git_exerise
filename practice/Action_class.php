468edea72df99bae0675ec56c9f2018707a7e2d0
xiz860817 <s0984008299@gmail.com>
2019-12-31 10:47:08 +0800

    test

--- practice/Action_class.php
+++ practice/Action_class.php
@@ -1,14 +1,7 @@
 <?php
-    include "Dog_class.php";
-    #include "Bird_class.php";
-    $dog = new Dog();
-    #$bird = new Bird();
     class Action{
         public function actionMove(Animal $odj){
             echo $odj->move();
         }
     }
-    $action = new Action();
-    $action->actionMove($dog);
-    #$action->actionMove($bird);
 ?>
