c742d48006893ee85a7d2bfcd11b16a89a3a112a
xiz860817 <s0984008299@gmail.com>
2019-12-31 10:51:37 +0800

    test

--- practice/demo5.php
+++ practice/demo5.php
@@ -3,8 +3,9 @@
     include "Bird_class.php";
     include "Action_class.php";
 
-    $dog = new Dog();
+    
     $bird = new Bird();
+    $dog = new Dog();
     $action = new action();
     $action->actionMove($dog);
     $action->actionMove($bird);
