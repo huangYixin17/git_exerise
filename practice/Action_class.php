712bee8137aebf04ddb2ca0b9c261f06d6b67185
xiz860817 <s0984008299@gmail.com>
2019-12-31 10:31:26 +0800

    test

--- practice/Action_class.php
+++ practice/Action_class.php
@@ -2,6 +2,7 @@
     include "Dog_class.php";
     include "Bird_class.php";
     include "Animal_abst.php";
+    $dog = new Dog();
     class Action{
         public function actionMove(Animal $dog){
             $dog->move();
