15f006531288d34cd85f5f2bbf5d532917d94d7d
xiz860817 <s0984008299@gmail.com>
2020-01-03 12:41:57 +0800

    多載

--- practice/person.php
+++ practice/person.php
@@ -1,15 +1,17 @@
 <?php
   class Person {
-      private $sex; #2.這裡可以被呼叫出來
-      private $ABC;  #1.這邊定義這個變數,如果不給他一個對應值,那沒辦法呼叫他
+      private $sex; #2.這裡可以被呼叫出來,即使沒有給他參數
+      public $ABC;  #1.這邊定義這個變數,如果不給他一個參數,那沒辦法呼叫他
+      #3.經果測試是因為public,private的差別
       public function __set($name, $value){
-        
+      #$name跟$value是可以被呼叫出來,即使沒有給他參數
         //限制不可動態產生屬性
-        if (isset($this->$name)) {
+        /*if (isset($this->$name)) {
             return $this->$name = $value;
         } else {
             return null;
-        }
+        }*/
+        echo "hello";
       }
       // 取得屬性名稱的值
       public function __get($name){
@@ -29,7 +31,6 @@
     //Person 類別沒有 name 這個屬性名稱
     //PHP_EOL 空隔或是換行
     $person->name = 'PHP';
-    
     echo $person->sex.PHP_EOL;
     echo $person->ABC.PHP_EOL;
     echo $person->name.PHP_EOL;
