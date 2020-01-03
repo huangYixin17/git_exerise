7cbb7cf119deaaba663f92018e5ca92ae348136d
xiz860817 <s0984008299@gmail.com>
2020-01-03 12:37:10 +0800

    test

--- practice/person.php
+++ practice/person.php
@@ -1,7 +1,7 @@
 <?php
   class Person {
       private $sex; #2.這裡可以被呼叫出來
-      public $ABC;  #1.這邊定義這個變數,如果不給他一個對應值,那沒辦法呼叫他
+      private $ABC;  #1.這邊定義這個變數,如果不給他一個對應值,那沒辦法呼叫他
       public function __set($name, $value){
         
         //限制不可動態產生屬性
@@ -29,7 +29,7 @@
     //Person 類別沒有 name 這個屬性名稱
     //PHP_EOL 空隔或是換行
     $person->name = 'PHP';
-    $person->ABC = "AB";
+    
     echo $person->sex.PHP_EOL;
     echo $person->ABC.PHP_EOL;
     echo $person->name.PHP_EOL;
